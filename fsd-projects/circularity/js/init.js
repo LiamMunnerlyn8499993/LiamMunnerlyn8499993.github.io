e9fc9973f307f4f83a780996de4e7f5bd3888745
LiamMunnerlyn8499993 <lem8499993@lpssonline.com>
2026-03-30 14:53:04 +0000

    todo 3 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -39,6 +39,15 @@ function drawCircle() {
 
         // TODO 3 : Call the drawCircle() function
 
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+
+drawCircle()
 
 
         // TODO 7 : Use a loop to create multiple circles
